fe160ab8fdee41d9e50de36bba187e036acaa3fb
krlzubiuk <36868164+krlzubiuk@users.noreply.github.com>
2020-02-01 20:59:08 +0200

    Add files via upload

--- hw/hw3/script.js
+++ hw/hw3/script.js
@@ -1,14 +1,28 @@
-let season = prompt("введите время года")
-seasons = [
-            "зима",
-            "зИма",
-            "Зима",
-            "ЗИма",
-            "лето",
-            "осень",
-            "весна"
-]
-if(seasons.some(function(i){
-return season===i
-})) alert("сейчас " +season)
-else alert("НЕВЕРНО")
+let discount=0.2
+let currency="uah"
+let i=1
+let q=1
+function calcDiscount() {
+    let productPrice = prompt("введите стоимость товара"+ " " + i++, "");
+    let productPriceWithDiscount = productPrice - (productPrice * discount);
+    console.log( "стоимость" +" " + q++ + " " + "товара со скидкой" + " " + productPriceWithDiscount + " " + currency);
+
+
+}
+
+
+
+calcDiscount()
+calcDiscount()
+calcDiscount()
+calcDiscount()
+calcDiscount()
+calcDiscount()
+calcDiscount()
+calcDiscount()
+calcDiscount()
+calcDiscount()
+calcDiscount()
+calcDiscount()
+calcDiscount()
+calcDiscount()
